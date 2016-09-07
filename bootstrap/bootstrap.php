a53d0c625809e0d2b8e321a4bded5ce23e405c11
Catalin08 <pocsanjr@gmail.com>
2016-09-07 19:21:43 +0300

    Inject application's settings

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -6,7 +6,7 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$app = new \Core\App();
+$app = new \Core\App(['basePath' => '/mvc-framework/public']);
 
 require_once __DIR__ . '/../app/routes.php';
 
