43e779f4c3eeb16d9e0784d5bd1b5772d1086659
Catalin08 <pocsanjr@gmail.com>
2016-09-07 06:40:05 +0300

    Define the required properties

--- core/Request.php
+++ core/Request.php
@@ -8,5 +8,27 @@ namespace Core;
 
 class Request
 {
+    /**
+     * Stores the content of the $_SERVER superglobal variable
+     * @var array
+     */
+    protected $server = [];
 
+    /**
+     * Stores the content of the $_POST superglobal variable
+     * @var array
+     */
+    protected $post = [];
+
+    /**
+     * Stores the content of the $_GET superglobal variable
+     * @var array
+     */
+    protected $query = [];
+
+    /**
+     * Stores the content of the $_FILES superglobal variable
+     * @var array
+     */
+    protected $file = []
 }
