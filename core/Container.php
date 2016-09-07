e4d73fa5460161396a605590baba39ecd5cd15ce
Catalin08 <pocsanjr@gmail.com>
2016-09-07 06:02:16 +0300

    Define the add() and get() method

--- core/Container.php
+++ core/Container.php
@@ -19,4 +19,19 @@ class Container
      * @var array
      */
     protected $cached = [];
+
+    /**
+     *  Add a new item in the $items property
+     * @param string $offset
+     * @param mixed $value
+     */
+    public function add(string $offset, $value) {}
+
+    /**
+     *  Get an item from the $items property 
+     *  If the item is found, cache that item and store it in the $cached property
+     * @param  string $offset
+     * @return mixed
+     */
+    public function get(string $offset) {}
 }
