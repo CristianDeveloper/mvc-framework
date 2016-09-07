88cd2a2b4f3a297dbe3a063477843faf83a8d485
Catalin08 <pocsanjr@gmail.com>
2016-09-07 17:48:50 +0300

    Add useful methods

--- core/Middleware.php
+++ core/Middleware.php
@@ -20,5 +20,56 @@ class Middleware
      */
     protected $globalMiddlewares = [];
 
-    
+    /**
+     * Holds the \Core\Container object
+     * @var object
+     */
+    protected $container;
+
+    public function __construct(\Core\Container $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * Add a route ( before ) middleware
+     * @param string $route      
+     * @param array  $middleware 
+     */
+    public function addMiddleware(string $route, array $middleware = []) 
+    {
+        $this->middlewares[$route] = $middleware;
+    }
+
+    /**
+     * Add a global ( before ) middleware
+     * @param array $globalMiddleware [description]
+     */
+    public function addGlobalMiddleware(array $globalMiddleware = [])
+    {
+        $this->globalMiddlewares[] = $globalMiddleware;
+    }
+
+    /**
+     * Call a route middleware based on the specified route
+     * @param  string $route  
+     */
+    public function callBeforeMiddleware(string $route)
+    {
+        if (isset($this->middlewares[$route])) {
+            $middleware = new $this->middlewares[$route][0]($this->container);
+            echo $middleware->{$this->middlewares[$route][1]}();
+        }
+    }
+
+    /**
+     * Call all the global middlewares
+     */
+    public function callGlobalMiddlewares()
+    {
+        foreach ($this->globalMiddlewares as $item) {
+            $middleware = new $item[0]($this->container);
+            echo $middleware->{$item[1]}();
+        }
+    }
 }
